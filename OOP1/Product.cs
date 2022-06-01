2bde7c5b25f653028011d1ccf4f1ad9afbdce2a9
tahiraltundag <tahiraltundag1@gmail.com>
2022-06-01 15:23:53 +0300

    Add project files.

---
+++ OOP1/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+
+        public Product(int id, int categoryId, string productName, double unitPrice, int unitsInStock)
+        {
+            Id = id;
+            CategoryId = categoryId;
+            ProductName = productName;
+            UnitPrice = unitPrice;
+            UnitsInStock = unitsInStock;
+        }
+
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product(1,2,"Masa",138.50,322);
+            Product product2 = new Product(2, 3, "Kalem", 32.45, 455);
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+        }
+    }
+}
